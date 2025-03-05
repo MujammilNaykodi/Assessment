2db0770f03e9d093246fe3269aa85868757fd87d
mujammil Naykodi <muzammilnaykodi92@gmail.com>
2025-03-05 17:59:31 +0530

    removed BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./componenets/Navbar";
 import Posts from "./pages/Posts";
 import Home from "./pages/Home"
